fda83f2cc9cf421ae7a7683d31154b00a91ae6a8
Asriel <63294282+Lari-skz@users.noreply.github.com>
2025-07-16 01:11:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function fetchQuote() {
   try {
-    const response = await fetch('/quote');
+    const response = await fetch('https://2082666d-df63-4fa3-b646-003f7a56e192-00-1y05tghu0sb3b.riker.replit.dev/quote');
     if (!response.ok) throw new Error('Network error');
     const data = await response.json();
 
