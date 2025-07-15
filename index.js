aa7ef2f798803bb7861ea3d20cf5cae0de23130e
Asriel <63294282+Lari-skz@users.noreply.github.com>
2025-07-16 00:47:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -46,5 +46,5 @@ app.get('/quote', (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () => console.log(`Proxy server running on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Proxy server running on port ${PORT}`));
