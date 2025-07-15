fc2324230ec4f669e87bff06fe7917e7397cead6
Asriel <63294282+Lari-skz@users.noreply.github.com>
2025-07-16 00:20:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function fetchQuote() {
   try {
-    const response = await fetch('https://api.quotable.io/random');
+    const response = await fetch('https://2082666d-df63-4fa3-b646-003f7a56e192-00-1y05tghu0sb3b.riker.replit.dev/');
     if (!response.ok) throw new Error('Network error');
     const data = await response.json();
     document.getElementById('quote').textContent = `"${data.content}"`;
